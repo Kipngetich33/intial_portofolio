c5226a3298be17d7693b2b35300cbccd060280f9
Vincent Ngeno <khalifngeno@gmail.com>
2017-11-06 08:31:14 +0300

    add google fonts ,social icons and other styling inclusions to the project

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#full-names").slideToggle();
+  $("#customized-menu").fadeOut();
+  $("#customized-menu").fadeIn();
+
+  $("#customized-menu").slideToggle();
+  $("#customized-menu").slideToggle();
+  
+// This section creates slide in effect for h4s
+  $("#slide-header").slideToggle();
+  $("#slide-paragraph").slideToggle();
+  $("#slide-header1").slideToggle();
+  $("#slide-paragraph1").slideToggle();
+  $("#slide-header3").slideToggle();
+
+});
